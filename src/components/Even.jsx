31cfc419c2094b979ee48628c66b6bd557b39328
sbag92879-code <sbag92879@gmail.com>
2026-06-17 23:45:34 +0900

    .

--- src/components/Even.jsx
+++ src/components/Even.jsx
@@ -2,8 +2,11 @@ import { useEffect } from "react";
 
 const Even = () => {
     useEffect(() => {
-        return () => {};
-    })
+        // 클린업, 정리함수
+        return () => {
+            console.log("unmount");
+        };
+    });
     return <div>짝수입니다</div>
 }
 
